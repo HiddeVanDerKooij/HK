e870177a6f58d55334f71295531e895bb51e2dbc
Hidde <hiddevanderkooij@gmail.com>
2024-10-27 21:50:27 +0100

    Platform timing API and linux support

--- HK/Util/Platform.h
+++ HK/Util/Platform.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "Common/Types.h"
+
 namespace Platform {
 	void SetDPIAware();
+	// TODO (HvdK): noexcept everywhere?
+	uint64 GetTicks() noexcept;
+	uint64 GetFrequency() noexcept;
 }
--- HK/Util/Platform_Linux.cpp
+++ HK/Util/Platform_Linux.cpp
@@ -4,8 +4,20 @@
 
 #include "Platform.h"
 
+#include <time.h>
+
 void Platform::SetDPIAware() {
 	// Do nothing
+}
+
+uint64 Platform::GetTicks() noexcept {
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return static_cast<uint64>(ts.tv_sec) * 1000000000ULL + static_cast<uint64>(ts.tv_nsec);
+}
+
+uint64 Platform::GetFrequency() noexcept {
+	return 1000000000ULL;
 }
 
 #endif
