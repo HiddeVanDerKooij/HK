cca591246a78debedd3ff56aaa9393f89634db19
Hidde van der Kooij <hiddevanderkooij@gmail.com>
2024-10-30 11:57:49 +0100

    GetTicks and GetFrequency for the Windows platform

--- HK/Util/Platform_Windows.cpp
+++ HK/Util/Platform_Windows.cpp
@@ -10,4 +10,16 @@ void Platform::SetDPIAware() {
 	SetProcessDPIAware();
 }
 
+uint64 Platform::GetTicks() noexcept {
+	LARGE_INTEGER li;
+	QueryPerformanceCounter(&li);
+	return li.QuadPart;
+}
+
+uint64 Platform::GetFrequency() noexcept {
+	LARGE_INTEGER li;
+	QueryPerformanceFrequency(&li);
+	return li.QuadPart;
+}
+
 #endif
