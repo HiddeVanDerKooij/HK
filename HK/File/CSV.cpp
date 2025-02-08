84a45db66220d74f19d8e094f49f413260f28e5c
Hidde <hiddevanderkooij@gmail.com>
2025-02-08 23:55:17 +0100

    New version of the CSV reader. Pass a structure as a template parameter and using one or two macros allow for easy mapping plus some additional rules of how to process the contents. It supports (u)int32s and stringviews

--- HK/File/CSV.cpp
+++ HK/File/CSV.cpp
@@ -4,31 +4,325 @@
 #include "CSV.h"
 #include "Bench/Bench.h"
 
-CSV::CSV()
-{}
+static String LastError;
 
-CSV::~CSV()
-{}
-
-CSV CSV::Create(StringView contents)
+void GCSVReader::AddPropertyInternal(StringView columnname,
+	const void* address, EPropertyType type, EPropertyMissingRules missingRule)
 {
-	CSV result;
+	ScopeBenchmark _f("GCSV::AddPropertyInternal"_sv);
 	
-	Array<StringView> lines = contents.Split('\n');
-	if (lines.Num() == 0) return result;
+	ColumnName column;
+	column.Name = columnname;
 	
-	Array<StringView> headers = lines[0].Split(',');
+	uint32* exists = ColumnNameToPropertyIndex.Find(column);
+	CHECK(exists == nullptr);
+	
+	ColumnNameToPropertyIndex.Add(column) = Properties.Num();
+	
+	Property property;
+	property.Name = columnname;
+	property.Type = type;
+	property.Address = address;
+	property.MissingRule = missingRule;
+	
+	Properties.Add(property);
+}
+
+bool GCSVReader::ParseHeader(StringView& csv, Array<int32>& outPropertyIndices) const
+{
+	ScopeBenchmark _f("GCSV::ParseHeader"_sv);
+	
+	int32 firstLine = csv.LeftFind('\n');
+	if (firstLine == -1)
+	{
+		LastError = "No newline found in CSV header"_sv;
+		return false;
+	}
+	
+	StringView header = csv.Substring(0, firstLine);
+	csv = csv.ChopLeft(firstLine + 1);
+	
+	Array<StringView> headers = header.Split(',');
+	Set<ColumnName> foundHeaders;
+	
 	for (uint32 i=0; i<headers.Num(); ++i)
 	{
-		result.Headers.Add({headers[i], i});
+		StringView h = headers[i];
+		h.Trim();
+		if (h.Size() == 0)
+		{
+			LastError = String::Format("The '{}'th header is empty"_sv, i);
+			return false;
+		}
+		
+		ColumnName column;
+		column.Name = h;
+		if (foundHeaders.Contains(column))
+		{
+			LastError = String::Format("Duplicate column '{}'"_sv, h);
+			return false;
+		}
+		foundHeaders.Add(column);
+		
+		const uint32* propertyIndex = ColumnNameToPropertyIndex.Find(column);
+		if (propertyIndex == nullptr)
+		{
+			outPropertyIndices.Add(-1);
+		}
+		else
+		{
+			outPropertyIndices.Add((int32)*propertyIndex);
+		}
 	}
 	
-	for (uint32 i=1; i<lines.Num(); ++i)
+	// We may have properties that are required but not in the CSV
+	for (uint32 i=0; i<Properties.Num(); ++i)
 	{
-		Array<StringView> columns = lines[i].Split(',');
-		if (columns.Num() != headers.Num()) continue;
-		result.Rows.Add({lines[i]});
+		if (Properties[i].MissingRule == EPropertyMissingRules::ClassDefault)
+			continue;
+		
+		if (!outPropertyIndices.Contains(i))
+		{
+			LastError = String::Format("Missing column '{}'"_sv, Properties[i].Name);
+			return false;
+		}
 	}
 	
-	return result;
+	return true;
+}
+
+bool GCSVReader::ParseRow(StringView& csv, uint32 rowCount, void* data,
+	const void* classDefault, const Array<int32>& propertyIndices, bool& bError) const
+{
+	int32 rowEnd = csv.LeftFind('\n');
+	if (rowEnd == -1)
+	{
+		rowEnd = csv.Size() - 1;
+	}
+	
+	if (rowEnd <= 0)
+	{
+		// End of file
+		return false;
+	}
+	
+	StringView row = csv.Substring(0, rowEnd);
+	csv = csv.ChopLeft(rowEnd + 1);
+	
+	uint32 currentColumn = 0;
+	
+	while (currentColumn < propertyIndices.Num())
+	{
+		StringView cell = StringView();
+		
+		while (StringUtil::IsWhitespace(row.FirstChar()))
+		{
+			row = row.ChopLeft(1);
+		}
+		
+		const uint8 firstChar = row.FirstChar();
+		bool bTrimCell = true;
+		
+		if (firstChar == '"')
+		{
+			// Parsing a quote
+			bTrimCell = false;
+			row = row.ChopLeft(1);
+			uint32 length = 0;
+			
+			while (true)
+			{
+				int32 quote = row.LeftFind('"', length);
+				if (quote == -1)
+				{
+					bError = true;
+					LastError = String::Format("No closing quote found in row '{}'"_sv, row);
+					return false;
+				}
+				
+				if (quote > 0 && row.At(quote - 1) == '\\')
+				{
+					length = quote + 1;
+					continue;
+				}
+				
+				cell = row.Substring(0, quote);
+				row = row.ChopLeft(quote + 1);
+				
+				while (StringUtil::IsWhitespace(row.FirstChar()))
+				{
+					row = row.ChopLeft(1);
+				}
+				if (row.FirstChar() == ',')
+				{
+					row = row.ChopLeft(1);
+				}
+				else if (row.FirstChar() == '\0')
+				{
+					row = StringView();
+				}
+
+				break;
+			}
+		}
+		else if (firstChar == ',')
+		{
+			// Empty cell
+			bTrimCell = false;
+			row = row.ChopLeft(1);
+		}
+		else if (firstChar == '\0')
+		{
+			// End of row
+			bTrimCell = false;
+			row = StringView();
+		}
+		else
+		{
+			// Parsing a regular cell
+			int32 comma = row.LeftFind(',');
+			int32 newline;
+			if (comma == -1)
+			{
+				cell = row;
+				row = StringView();
+			}
+			else
+			{
+				cell = row.Substring(0, comma);
+				row = row.ChopLeft(comma + 1);
+			}
+		}
+		
+		if (bTrimCell)
+		{
+			cell.Trim();
+		}
+		
+		int32 propertyIndex = propertyIndices[currentColumn];
+		if (propertyIndex != -1)
+		{
+			const Property& property = Properties[propertyIndex];
+			
+			bool bRequired = property.MissingRule == EPropertyMissingRules::Error;
+			if (property.MissingRule == EPropertyMissingRules::CopyPrevious && rowCount == 0)
+			{
+				bRequired = true;
+			}
+			
+			union {
+				int32 i;
+				uint32 u;
+				f32 f;
+				StringView s;
+			} value = {};
+			
+			uint8 propertyBytes = 0;
+			bool bParsed = true;
+			
+			if (property.Type == EPropertyType::Int32 ||
+				property.Type == EPropertyType::UInt32)
+			{
+				int64 result = 0;
+				if (!cell.ParseAsInt(result, 10))
+				{
+					bParsed = false;
+					if (bRequired)
+					{
+						bError = true;
+						LastError = String::Format("Cannot parse '{}' as int"_sv, cell);
+						return false;
+					}
+				}
+				else
+				{
+					int64 min = int64(property.Type == EPropertyType::Int32 ? LIMIT_INT32_MIN : 0);
+					int64 max = int64(property.Type == EPropertyType::Int32 ? LIMIT_INT32_MAX : LIMIT_UINT32_MAX);
+					
+					if (result < min || result > max)
+					{
+						bError = true;
+						LastError = String::Format("Value '{}' out of range for int"_sv, result);
+						return false;
+					}
+					
+					value.i = (int32)result;
+				}
+				propertyBytes = 4;
+			}
+			else if (property.Type == EPropertyType::Float)
+			{
+				// TODO (HvdK)
+				bError = true;
+				LastError = "Float parsing not yet supported"_sv;
+				return false;
+			}
+			else if (property.Type == EPropertyType::StringView)
+			{
+				if (cell.Size() == 0)
+				{
+					bParsed = false;
+					if (bRequired)
+					{
+						bError = true;
+						LastError = "Empty string not allowed"_sv;
+						return false;
+					}
+				}
+				else
+				{
+					value.s = cell;
+				}
+				propertyBytes = sizeof(StringView);
+			}
+			else
+			{
+				bError = true;
+				LastError = "Unknown property type"_sv;
+				return false;
+			}
+			
+			if (bParsed)
+			{
+				Memory::Copy(&value, reinterpret_cast<uint8*>(data) + (uint64)property.Address, propertyBytes);
+			}
+			else if (property.MissingRule == EPropertyMissingRules::ClassDefault)
+			{
+				Memory::Copy(reinterpret_cast<const uint8*>(classDefault)+ (uint64)property.Address, reinterpret_cast<uint8*>(data) + (uint64)property.Address, propertyBytes);
+			}
+			// If missing rule is CopyPrevious, we don't need to do anything
+		}
+		
+		currentColumn += 1;
+	}
+	
+	if (row.Trimmed().Size() > 0)
+	{
+		bError = true;
+		LastError = String::Format("Unexpected data in row: '{}'"_sv, row);
+		return false;
+	}
+	
+	return true;
+}
+
+StringView GCSVReader::GetError()
+{
+	return LastError.AsView();
+}
+
+void GCSVReader::ColumnName::Hash(Hasher<uint32>& hasher) const
+{
+	hasher.Add(Name.Size());
+	const char8* ptr = Name.Data();
+	const uint32 size = Name.Size();
+	for (uint32 i=0; i<size; ++i)
+	{
+		hasher.Add(ptr[i]);
+	}
+}
+
+bool GCSVReader::ColumnName::operator==(const ColumnName& other) const
+{
+	return Name == other.Name;
 }
--- HK/File/CSV.h
+++ HK/File/CSV.h
@@ -5,32 +5,127 @@
 
 #include "Common/Types.h"
 #include "Containers/Array.h"
+#include "Containers/HashMap.h"
 #include "Containers/View/StringView.h"
+#include "Strings/String.h"
 
-class CSV
+#define CSV_ADD_PROPERTY(_csv, _column, _member) _csv.AddProperty(_column, GCSVReader::EPropertyMissingRules::Error, ADDRESS_OF(decltype(_csv)::PropertyType, _member))
+#define CSV_ADD_PROPERTY_EXT(_csv, _column, _member, _missing) _csv.AddProperty(_column, _missing, ADDRESS_OF(decltype(_csv)::PropertyType, _member))
+
+class GCSVReader
 {
+public:
+	enum class EPropertyMissingRules
+	{
+		ClassDefault,
+		Error,
+		CopyPrevious
+	};
+	
 protected:
-	CSV();
-	~CSV();
+	enum class EPropertyType
+	{
+		Int32,
+		UInt32,
+		Float,
+		StringView,
+	};
+	
+	void AddPropertyInternal(StringView column, const void* address, EPropertyType type, EPropertyMissingRules missingRule);
+	
+	// Returns false if the header is invalid
+	// Stores the error in a global variable you can get with
+	// GetError()
+	// Mutates csv to point to the first row
+	bool ParseHeader(StringView& csv, Array<int32>& outPropertyIndices) const;
+	bool ParseRow(StringView& csv, uint32 rowCount, void* data, const void* classDefault, const Array<int32>& propertyIndices, bool& bError) const;
 	
 public:
-	static CSV Create(StringView contents);
+	static StringView GetError();
 	
-	bool GetColumnIndex(StringView title, uint32& outIndex) const;
-	StringView At(uint32 row, uint32 column) const;
-	
-protected:
-	struct Header
+private:
+	struct ColumnName
 	{
-		StringView Title;
-		uint32 Index;
+		StringView Name;
+		
+		void Hash(Hasher<uint32>& hasher) const;
+		bool operator==(const ColumnName& other) const;
+	};
+
+	struct Property
+	{
+		StringView Name;
+		EPropertyType Type;
+		const void* Address;
+		EPropertyMissingRules MissingRule;
 	};
 	
-	struct Row
-	{
-		StringView Contents;
-	};
-	
-	Array<Header> Headers;
-	Array<Row> Rows;
+	HashMap<ColumnName, uint32> ColumnNameToPropertyIndex;
+	Array<Property> Properties;
 };
+
+template<typename T>
+class CSVReader : public GCSVReader
+{
+public:
+	typedef T PropertyType;
+	
+	void AddProperty(StringView column, EPropertyMissingRules missingRule, const int32* address);
+	void AddProperty(StringView column, EPropertyMissingRules missingRule, const uint32* address);
+	void AddProperty(StringView column, EPropertyMissingRules missingRule, const f32* address);
+	void AddProperty(StringView column, EPropertyMissingRules missingRule, const StringView* address);
+	
+	Array<T> GetRows(StringView csv) const;
+};
+
+template<typename T>
+void CSVReader<T>::AddProperty(StringView column, EPropertyMissingRules missingRule, const int32* address)
+{
+	AddPropertyInternal(column, address, EPropertyType::Int32, missingRule);
+}
+
+template<typename T>
+void CSVReader<T>::AddProperty(StringView column, EPropertyMissingRules missingRule, const uint32* address)
+{
+	AddPropertyInternal(column, address, EPropertyType::UInt32, missingRule);
+}
+
+template<typename T>
+void CSVReader<T>::AddProperty(StringView column, EPropertyMissingRules missingRule, const f32* address)
+{
+	AddPropertyInternal(column, address, EPropertyType::Float, missingRule);
+}
+
+template<typename T>
+void CSVReader<T>::AddProperty(StringView column, EPropertyMissingRules missingRule, const StringView* address)
+{
+	AddPropertyInternal(column, address, EPropertyType::StringView, missingRule);
+}
+
+template<typename T>
+Array<T> CSVReader<T>::GetRows(StringView csv) const
+{
+	// ScopeBenchmark _f("CSVReader<T>::GetRows"_sv);
+	
+	Array<int32> propertyIndices;
+	if (!ParseHeader(csv, propertyIndices))
+	{
+		return Array<T>();
+	}
+	
+	Array<T> result;
+	
+	const T classDefault = T();
+	T row = classDefault;
+	bool bError = false;
+	while (ParseRow(csv, result.Num(), &row, &classDefault, propertyIndices, bError))
+	{
+		if (bError)
+		{
+			return Array<T>();
+		}
+		result.Add(row);
+	}
+	
+	return result;
+}
